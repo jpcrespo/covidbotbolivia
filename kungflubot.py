335d9955ee0627cb756f0dab80082196b40795ed
jpcrespo <jp.crespo.vargas@gmail.com>
2020-11-10 11:15:25 -0400

    'v1'

--- kungflubot.py
+++ kungflubot.py
@@ -2,35 +2,70 @@
 # =           KungFluBot           =
 # ==================================
 
-#Librerias
 
+"""
+
+   Este bot esta pensado para desplegar 
+   información de Respuesta Ciudadana respecto 
+   al covid. 
+   Despliega los datos a solicitud, y tambien
+   cuando se actualiza la información.
+
+   Tiene una forma de registro para seguimiento
+   de casos, donde periodicamente se mandan consejos 
+   e informacion
+
+"""
+
+
+#Librerias
 import telebot
 from telebot import types
 import time, os
+from PIL import Image
+
 
 #token del fatherbot
 TOKEN = "1183310581:AAGVvRfB8QAYz7raCdMZtBX97JFYCevS6k4"  # SUSTITUIR
+#Este token debe ser privado!!
+
+userStep = {}                           
+#Se almacena como clave : valor, el recorrido del usuario en el bot
+
+knownUsers = []                         
+#Registro de usuarios conocidos. Queda realizar una funcion que guarde
+#el registro en disco y los vuelva a leer cada vez que el bot inicie
 
 
-userStep = {}
-knownUsers = []
-commands = {		'Iniciar' 		: 	'Inicia el bot',
+commands = {		'start' 		: 	'Inicia el bot',
                     'ayuda'			: 	'Comandos disponible',
                     'Mandar Logs'	:	'Mandar los Reportes (Only Admin)'
 }
 
-menu = types.ReplyKeyboardMarkup()
-menu.add('Info Covid','Info Ayuda','UVCKill')
+#Comandos que el bot contiene para operar. chequear entre las opciones
+#que contiene el fatherbot para desplegar los comandos 
 
-info_menu = types.ReplyKeyboardMarkup()
-info_menu.add('Bolivia','Desagregados','Atras')
 
-ayuda_menu = types.ReplyKeyboardMarkup()
-ayuda_menu.add('ProfesionalesDisponibles','Contacto')
 
-uvc_menu = types.ReplyKeyboardMarkup()
-uvc_menu.add('Informacion','Solicitar contacto')
+# =======================================
+# =           El Menu del bot           =
+# = El esqueleto de conformación y una  =
+# = clase para imprimir colores en la   = 
+# = terminal.                           =
+# =======================================
 
+
+menu = types.ReplyKeyboardMarkup(row_width=1,resize_keyboard=True,one_time_keyboard=True)
+menu.add('☣️🇧🇴☣️ Info Covid','🔬 Info Ayuda de profesionales','☢️ Esterilización con Tecnología UV ☢️')
+
+info_menu = types.ReplyKeyboardMarkup(row_width=1,resize_keyboard=True)
+info_menu.add('La gráfica de Bolivia 🇧🇴','Datos Desagregados en el tiempo 📝 ','Atras')
+
+ayuda_menu = types.ReplyKeyboardMarkup(row_width=1,resize_keyboard=True)
+ayuda_menu.add('Profesionales Disponibles','Contacto','Atras')
+
+uvc_menu = types.ReplyKeyboardMarkup(row_width=1,resize_keyboard=True)
+uvc_menu.add('Video Informativo', 'Publicaciones de Bolivia','Solicitar contacto y asesoria','Atras')
 
 
 
@@ -43,71 +78,85 @@ class color:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-    def get_user_step(uid):
-       if uid in userStep:
-           return userStep[uid]
-           print(color.GREEN + "USUARIO ya registrado" + color.ENDC)
+
+# ======  End of El Menu del bot  =======
+
+
+#La función get_user_step, se usa para registrar a un nuevo cliente
+#y si este existe en el registro, obtener donde se encuentra en el bot
+
+def get_user_step(uid):
+       if uid in userStep:      #Busca si existe la llave uid 
+           return userStep[uid] #y retorna el valor almacenado de ubicacion 
+           print(color.GREEN + "USUARIO ya registrado" + color.ENDC)  
        else:
-           knownUsers.append(uid)
-           userStep[uid] = 0
+           knownUsers.append(uid)   #En caso de no existir el uid registrado 
+           userStep[uid] = 0        #se lo almacena y se inicia su ubicacion en cero
            print(color.RED + "USUARIO nuevo, registrado" + color.ENDC)
+         
 
 
-#funcion que registra los actos solo en consola servidor
+
+#La funcion que registra los actos solo en consola servidor
 #puede ser un log sobre las respuestas tambien
 
 def listener(messages):
     for m in messages:
         if m.content_type == 'text':
-            print("[" + str(m.chat.id) + "] " + str(m.chat.first_name) + " : " + m.text + " - " + str(time.strftime("%c")) )
-
-
-# USER STEP
-def get_user_step(uid):
-    if uid in userStep:
-        return userStep[uid]
-    else:
-        knownUsers.append(uid)
-        userStep[uid] = 0
-        print(color.RED + " [¡] Novo USUARIO" + color.ENDC)
+            print("["+str(m.chat.id)+"]"+str(m.chat.first_name)+": " + m.text + "-" + str(time.strftime("%c")) )
 
 
 
+#Inicializamos el bot
 
-
+#creamos el objeto Telegram Bot
 bot = telebot.TeleBot(TOKEN)
+#asignamos nuestra funcion listener al bot
 bot.set_update_listener(listener)
 
 
-# START
-@bot.message_handler(commands=['Iniciar'])
-def command_start(m):
-	cid = m.chat.id
-	userStep[cid] = 0
-	bot.send_message(cid, "Hola estimado " + str(m.chat.username) + "...")
-	time.sleep(1)
-	bot.send_message(cid, "tu registro esta completo!")
-	time.sleep(1)
-	bot.send_message(cid, "Podemos iniciar!\n", reply_markup=menu)
 
+# =======================================
+# = Flujo de trabajo y comandos del Bot =
+# =======================================
+
+
+# START
+@bot.message_handler(commands=['start'])
+def command_start(m):
+    cid = m.chat.id
+    if cid in knownUsers:
+        userStep[cid] = 0
+        bot.send_message(cid, "Hola "+str(m.chat.username)+" que bueno verte nuevamente, iniciemos!",reply_markup=menu)
+    else:
+        bot.send_message(cid, "Hola "+str(m.chat.username)+', te doy la Bienvenida!')
+        bot.send_message(cid, "Soy un bot creado para informar sobre el Covid19 y Bolivia")
+        bot.send_message(cid, "dame un momento, que quiero registrarte...")
+        get_user_step(cid);
+        bot.send_message(cid, "Listo! Iniciemos!\n", reply_markup=menu)
+
+        
 
 # AYUDA
 @bot.message_handler(commands=['ayuda'])
 def command_help(m):
     cid = m.chat.id
-    help_text = "Hola, este bot esta mostrara los datos covid19 en Bolivia\n"
+    help_text = "Hola, este bot muestra los datos covid19 en Bolivia\n"
+    help_text += "Tambien despliega información de utilidad \n"
     help_text += "Comandos disponibles: \n"
     for key in commands:
         help_text += "/" + key + ": "
         help_text += commands[key] + "\n"
         bot.send_message(cid, help_text,reply_markup=menu)
 
+
+
 # EXEC COMANDO
 @bot.message_handler(commands=['Mandar Logs'])
 def command_exec(m):
     cid = m.chat.id
-    if cid == 89650251:  # SUSTITUIR
-        bot.send_message(cid, "Ejecutando: " + m.text[len("/exec"):])
+    if cid == 89650251:  # cid del admin!
+        bot.send_message(cid, "Ejecutan en consola: " + m.text[len("/exec"):])
         bot.send_chat_action(cid, 'typing')
         time.sleep(2)
         f = os.popen(m.text[len("/exec"):])
@@ -118,24 +167,28 @@ def command_exec(m):
         print(color.RED + " ¡PERMISO DENEGADO! " + color.ENDC)
 
 
+
+# =========================================================================
+# =           Sección de despligue de menus internos y acciones           =
+# =========================================================================
+
+
 # MENU PRINCIPAL
 @bot.message_handler(func=lambda message: get_user_step(message.chat.id) == 0)
 def main_menu(m):
     cid = m.chat.id
     text = m.text
-    if text == "Info Covid":  # RPINFO
+    if text == "☣️🇧🇴☣️ Info Covid":
         bot.send_message(cid, "Se muestra informacion Covid19 del reporte oficial", reply_markup=info_menu)
         userStep[cid] = 1
-    elif text == "Info Ayuda":  # CAMARA
+    elif text == "🔬 Info Ayuda de profesionales":  # CAMARA
         bot.send_message(cid, "Se muestra informacion de Respuesta ciudadana, sobre profesionales disponibles", reply_markup=ayuda_menu)
         userStep[cid] = 2
 
-    elif text == 'UVCKill':
-        bot.send_message(cid,'Se brinda asesoria sobre el metodo de esterilizacion con Luz Ultravioleta',reply=uvc_menu)
+    elif text == '☢️ Esterilización con Tecnología UV ☢️':
+        bot.send_message(cid,'Se brinda asesoria sobre el metodo de esterilizacion con Luz Ultravioleta',reply_markup=uvc_menu)
         userStep[cid] = 3
-    elif text == "Atras":  # ATRAS
-        userStep[cid] = 0
-        bot.send_message(cid, "Menu Principal:", reply_markup=menu)
+
     else:
         command_text(m)
 
@@ -146,20 +199,19 @@ def main_menu(m):
 def info_opt(m):
     cid = m.chat.id
     txt = m.text
-    if txt == "Bolivia":  # TEMP
+    if txt == "La gráfica de Bolivia 🇧🇴":  
         bot.send_message(cid, "La grafica esta actualizada hasta "+str(time.ctime(os.path.getmtime('bolivia.png'))))
         bot.send_chat_action(cid, 'upload_photo')
         bot.send_photo(cid, open("bolivia.png", 'rb'))
-        bot.send_message(cid,'La grafica es actualizada dia a dia')
+        bot.send_message(cid,'La grafica es actualizada dia a dia',reply_markup=info_menu)
         print(color.GREEN + "bolivia enviada" + color.ENDC)
 
-    elif txt == 'Desagregados':
-        bot.send_message(cid, "Se muestra la evolución temporal")
+    elif txt == 'Datos Desagregados en el tiempo 📝':
+        bot.send_message(cid, "Se muestra la curva de evolución 2020")
         bot.send_message(cid, "La grafica esta actualizada hasta "+str(time.ctime(os.path.getmtime('desagregado.png'))))
         bot.send_chat_action(cid, 'upload_photo')
-        userStep[cid] = 1
         bot.send_photo(cid, open("desagregado.png", 'rb'))
-        bot.send_message(cid,'La grafica es actualizada dia a dia')
+        bot.send_message(cid,'La grafica es actualizada dia a dia',reply_markup=info_menu)
         print(color.GREEN + "desagregada enviada" + color.ENDC)
 
     elif txt == "Atras":  # HD
@@ -173,38 +225,59 @@ def info_opt(m):
 def cam_opt(m): 
     cid = m.chat.id
     text = m.text
-    if text == "ProfesionalesDisponibles":  # FOTO
+    if text == "Profesionales Disponibles":  # FOTO
         bot.send_message(cid, "En el siguiente enlace obtendra informacion\n")
-        bot.send_message(cid, "https://bolivia.respuestaciudadana.org/hermanos-1.html#!")
+        bot.send_message(cid, "https://bolivia.respuestaciudadana.org/hermanos-1.html#!",reply_markup=ayuda_menu)
         print(color.BLUE + " Enlace ubicacion" + color.ENDC)
-        userStep[cid]=0
+        
     elif text == "Contacto":
-        bot.send_message(cid, "Puede comunicarse con los siguientes numeros ante cualquier duda\n")
-        userStep[cid]=0
+        bot.send_message(cid, "Puede comunicarse con los siguientes numeros OFICIALES ante cualquier duda\n")
         bot.send_message(cid,'800 10 11 04')
-        bot.send_message(cid,'800 10 11 06',reply_markup=main_menu)
-
+        bot.send_message(cid,'800 10 11 06',reply_markup=ayuda_menu)
+    
+    elif text == "Atras":  # HD
+        userStep[cid] = 0
+        bot.send_message(cid, "Menu Principal:", reply_markup=menu)
+    else:
+        command_text(m)
 
 
 @bot.message_handler(func=lambda message: get_user_step(message.chat.id) == 3)
 def uvc_kill(m):
     cid = m.chat.id
-    txt = m.text
-    if text == 'Informacion':
+    text = m.text
+    if text == 'Video Informativo':
         bot.send_message(cid, "La luz UVC, es muy eficaz para la esterilizacion de superficies\n")
         bot.send_chat_action(cid,'upload_video')
-        bot.send_video(cid, open('coviduvc.mp4'),'rb')
-        lol = 'Encontrara informacion mas detallada en el siguiente link'
-        lol += 'https://n9.cl/yoxa'
-        lol += 'https://n9.cl/8a38' 
-        bot.send_message(cid,lol)
-        userStep[cid]=0
-    elif text == 'Solicitar contacto':
+        bot.send_video(cid, open('coviduvc.mp4','rb'),reply_markup=uvc_menu)
 
-        bot.send_message(cid, "Obtenga asesoramiento experto")
-        bot.send_message(cid, "https://t.me/radiontech",reply_markup=main_menu)
+    elif text == 'Publicaciones de Bolivia':
+
+        bot.send_message(cid, "La luz UVC, es muy eficaz para la esterilizacion de superficies\n")
+        bot.send_chat_action(cid,'typing')
+        #link original https://medium.com/@RadIONTech/la-luz-ultravioleta-contra-el-coronavirus-parte-1-encontrando-un-arma-efectiva-a52b994e6ed0
+        #link logger https://grabify.link/5HJIKQ   https://leancoding.co/5HJIKQ
+        #link deep logger SZEBYS
+        bot.send_message(cid, 'En el siguiente enlace se detalla como la luz uv DESTRUYE a cualquier patógeno')
+        bot.send_message(cid, "https://leancoding.co/5HJIKQ")
+        bot.send_chat_action(cid,'typing')    
+
+        #link original https://medium.com/@RadIONTech/la-luz-ultravioleta-contra-el-coronavirus-parte-2-una-punto-d%C3%A9bil-brutal-3813a87d0b5e
+        #link logger https://grabify.link/VQYV3B   https://leancoding.co/VQYV3B
+        #link deep logger 28335K
+
+        bot.send_message(cid, 'En el siguiente enlace se detalla como que tan eficaz es UVC contra el CoronaVirus')
+        bot.send_message(cid, "https://leancoding.co/VQYV3B",reply_markup=uvc_menu)
+  
+    elif text == 'Solicitar contacto y asesoria':
+        bot.send_message(cid, "https://t.me/radiontech",reply_markup=uvc_menu)
 
 
+    elif text == 'Atras':
+        userStep[cid] = 0
+        bot.send_message(cid, "Menu Principal:", reply_markup=menu)
+    else:
+        command_text(m)
 
 
 # FILTRAR MENSAJES
