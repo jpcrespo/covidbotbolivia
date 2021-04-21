43845056d5b3477029f7b33f66a2e6f531443cb7
jpcrespo <jp.crespo.vargas@gmail.com>
2021-04-20 20:16:09 -0400

    auto

---
+++ down.py
@@ -0,0 +1,26 @@
+import telebot, json
+from telebot import types
+
+import time, os, sys
+
+sys.path.insert(0, 'core/')
+import datos
+from datos import *
+
+
+# aux         = np.load('bins/knownUsers.npy', allow_pickle='TRUE') 
+# knownUsers  = aux.tolist()
+
+knownUsers = [89650251]
+
+bot = telebot.TeleBot(token)
+
+
+
+
+if __name__ == '__main__':
+   for us in knownUsers:
+   	bot.send_message(us, '🤖 El bot entra en mantenimiento, se actualizan los datos y se crean nuevas instancias.\nVuelvo en 5 min 🏃‍♂',disable_notification= True )
+   sys.exit(0)
+
+
---
+++ up.py
@@ -0,0 +1,26 @@
+import telebot, json
+from telebot import types
+
+import time, os, sys
+
+sys.path.insert(0, 'core/')
+import datos
+from datos import *
+
+
+# aux         = np.load('bins/knownUsers.npy', allow_pickle='TRUE') 
+# knownUsers  = aux.tolist()
+
+knownUsers = [89650251]
+
+bot = telebot.TeleBot(token)
+
+
+
+
+if __name__ == '__main__':
+   for us in knownUsers:
+   	bot.send_message(us, '🤖 El bot esta nuevamente en funcionamiento 👌',disable_notification= True )
+   sys.exit(0)
+
+
