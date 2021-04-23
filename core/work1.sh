84817d2707d56b2a5ff7ae4c5620eedd5128d89f
rpiCrespo <jp.crespo.vargas@gmail.com>
2021-04-23 16:00:46 -0400

    v1.2

--- core/work1.sh
+++ core/work1.sh
@@ -4,7 +4,7 @@ cd core/
 sudo rm -r vacunas covid19-bolivia pics
 git clone https://github.com/mauforonda/vacunas
 git clone -b opsoms https://github.com/mauforonda/covid19-bolivia
-
+mkdir pics
 cd ..
 cd ..
 
