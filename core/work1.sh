325f4777ea304767b2fe220b07fb27188433aa28
rpiCrespo <jp.crespo.vargas@gmail.com>
2021-04-23 01:00:51 -0400

    new bug

---
+++ core/work1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd core/
+sudo rm -r vacunas covid19-bolivia pics
+git clone https://github.com/mauforonda/vacunas
+git clone -b opsoms https://github.com/mauforonda/covid19-bolivia
+
+cd ..
+cd ..
+
+source bin/activate
+
+
+cd /home/pi/Desktop/telebot/covidbotbolivia/core/
+
+python recopilador.py
+python notifi.py
+
+
